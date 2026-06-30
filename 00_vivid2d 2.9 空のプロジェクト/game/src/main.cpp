e10f5cb42b5c3bfff8d196b70b9dfe90cd8fdf61
nago-75-0522 <oibc37072@ojs.ac.jp>
2026-06-30 09:48:34 +0900

    Add files via upload

---
+++ 00_vivid2d 2.9 空のプロジェクト/game/src/main.cpp
@@ -0,0 +1,51 @@
+
+/*!
+ *  @file       main.cpp
+ *  @brief      エントリーポイント
+ *  @author     Kazuya Maruyama
+ *				Toshihito Ohashi
+ *  @date       2023/05/08
+ *  @version    1.1
+ *
+ *  Copyright (c) 2013-2023, Kazuya Maruyama. Toshihito Ohashi. All rights reserved.
+ */
+
+#include "vivid.h"
+
+/*!
+ *  @brief      描画関数
+ */
+void
+Display(void)
+{
+}
+
+/*!
+ *  @brief      メイン関数
+ *
+ *  @param[in]  hInst       インスタンスハンドル
+ *  @param[in]  hPrevInst   Win16時代の遺物
+ *  @param[in]  lpCmdLine   コマンドライン文字列
+ *  @param[in]  nCmdShow    ウィンドの表示方法
+ *
+ *  @return     メイン関数の成否
+ */
+int WINAPI
+WinMain( _In_ HINSTANCE hInst, _In_opt_ HINSTANCE hPrevInst, _In_ LPSTR lpCmdLine, _In_ int nCmdShow )
+{
+    (void)hPrevInst;
+    (void)lpCmdLine;
+    (void)nCmdShow;
+
+    // vividライブラリ初期化
+    vivid::Initialize( hInst );
+
+    // 更新/描画関数登録
+    vivid::DisplayFunction( Display );
+
+    // ゲームループ
+    vivid::MainLoop( );
+
+    // vividライブラリ解放
+    vivid::Finalize( );
+}
