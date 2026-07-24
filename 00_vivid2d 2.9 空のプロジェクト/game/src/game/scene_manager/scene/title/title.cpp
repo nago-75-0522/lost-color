e6dc6e63661e0513cfdfbc90e1ddd8c583e4e261
nago-75-0522 <oibc37072@ojs.ac.jp>
2026-07-24 15:23:35 +0900

    title画面中の処理
    動画の再生
    ：今後やること　タイトルに動きをつける
    ：　　　　　　音をつける

--- 00_vivid2d 2.9 空のプロジェクト/game/src/game/scene_manager/scene/title/title.cpp
+++ 00_vivid2d 2.9 空のプロジェクト/game/src/game/scene_manager/scene/title/title.cpp
@@ -4,16 +4,27 @@
 #include"../color_select/color_select.h"
 
 CTitle::CTitle()
+	:m_Logo_Time(0)
+	,m_title_logo_file("data\\logo\\ge-mulogo1.png")
+	,m_title_logo_width(1000)
+	,m_title_logo_height(500)
 {
 }
 
 void CTitle::Initialize(void)
 {
+	
+	//画像の位置
+	m_TitlePos.x = vivid::WINDOW_WIDTH / 2 - (m_title_logo_width/2);
+	m_TitlePos.y = vivid::WINDOW_HEIGHT / 2 - (m_title_logo_height / 2);
+
 
 }
 
 void CTitle::Update(void)
 {
+	++m_Logo_Time;
+
 	CSceneManager::GetInstance().ResetStageCount();//カウントリセット
 
 	//キーボード用
@@ -28,12 +39,48 @@ void CTitle::Update(void)
 		CColor_Select::GetInstance().IniColor();
 		CSceneManager::GetInstance().Change(SCENE_ID::OPTION);
 	}
+
 }
 
 void CTitle::Draw(void)
 {
+
 	vivid::DrawText(48, "title", { 0.0f,0.0f });
-	vivid::DrawTexture("data\\title_lf.png", { 100,vivid::WINDOW_HEIGHT * 1 / 5 });
+	
+	vivid::Rect rect = { 0,0,0,0 };
+	rect.right = m_title_logo_width;
+	rect.bottom = m_title_logo_height;
+
+
+	//タイトルロゴの表示時間の計算 if(条件)
+	if (m_Logo_Time > 10 * 60 )
+	{
+		/* タイトル動画	 動画再生中にどこかのキーを押すとタイトルに戻る　スペース押すと次の処理行く */
+		PlayMovie("data\\db.mp4", 1, DX_MOVIEPLAYTYPE_BCANCEL);//DX_MOVIEPLAYTYPE_BCANCEL　キー入力あり
+
+		//キーボード用
+		if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::SPACE))
+		{
+			CColor_Select::GetInstance().IniColor();
+			CSceneManager::GetInstance().Change(SCENE_ID::OPTION);
+		}
+		//コントローラー用
+		if (vivid::controller::Trigger(vivid::controller::DEVICE_ID::PLAYER1, vivid::controller::BUTTON_ID::B))
+		{
+			CColor_Select::GetInstance().IniColor();
+			CSceneManager::GetInstance().Change(SCENE_ID::OPTION);
+		}
+
+		else
+		{
+			//タイマーリセット
+			m_Logo_Time = 0;
+		}
+
+	}
+
+	vivid::DrawTexture(m_title_logo_file, m_TitlePos);
+
 }
 
 void CTitle::Finalize(void)
--- 00_vivid2d 2.9 空のプロジェクト/game/src/game/scene_manager/scene/title/title.h
+++ 00_vivid2d 2.9 空のプロジェクト/game/src/game/scene_manager/scene/title/title.h
@@ -24,6 +24,19 @@ private:
 	CTitle& operator=(const CTitle& rhp) = delete;
 
 	~CTitle(void) = default;
-	//ロゴスタートの位置
-	vivid::Vector2 m_start_logo;
+
+	const std::string m_title_logo_file;		//ロゴ画像
+	const int		  m_title_logo_width;		//幅
+	const int		  m_title_logo_height;		//高さ
+
+	//vivid::Vector2		  m_title_logo_anchor;		//中心点
+	vivid::Vector2			  m_TitlePos;			//タイトル画像表示位置
+
+
+	int m_Logo_Time;						//ロゴの表示時間
+	
+	vivid::Vector2 m_start_logo;			//ロゴスタートの位置
+
+	
+
 };
